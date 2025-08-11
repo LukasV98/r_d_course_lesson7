6975ab6c732e2c8878f989441d9aaa2c1c02aa45
Lukáš Vrba <lukas.vrba@eu.panasonic.com>
2025-08-11 20:31:39 +0200

    deploy

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,9 @@
-output "instance_public_ip" {
-  value = ""                                          # The actual value to be outputted
-  description = "The public IP address of the EC2 instance" # Description of what this output represents
+output "load_balancer_dns" {
+  description = "DNS name of the load balancer"
+  value = aws_lb.main.dns_name
+}
+
+output "load_balancer_url" {
+  description = "URL of the load balancer"
+  value = "http://${aws_lb.main.dns_name}"
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  default = "eu-central-1"
+  type = string
+  description = "AWS region"
+}
+
+variable "project_name" {
+  default = "ecs-nginx-demo"
+}

848a88cbd17155229f32f003ff538f5f9277e122
Lukáš Vrba <lukas.vrba@eu.panasonic.com>
2025-08-13 10:54:01 +0200

    lesson8 1.0

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,18 @@ output "load_balancer_url" {
   description = "URL of the load balancer"
   value = "http://${aws_lb.main.dns_name}"
 }
+
+output "instance_public_ip" {
+  value = ""                                          # The actual value to be outputted
+  description = "The public IP address of the EC2 instance" # Description of what this output represents
+}
+
+output "ecr_repository_url" {
+description = "URL of the ECR repository"
+value = aws_ecr_repository.custom_web.repository_url
+}
+
+output "ecr_repository_arn" {
+description = "ARN of the ECR repository"
+value = aws_ecr_repository.custom_web.arn
+}

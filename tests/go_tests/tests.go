b38c42efdaad87171910c93239ca4eb2e3b54ad8
Lukáš Vrba <lukas.vrba@eu.panasonic.com>
2025-08-13 15:26:37 +0200

    lesson9

---
+++ tests/go_tests/tests.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+
+	"github.com/tebeka/selenium"
+	"github.com/tebeka/selenium/chrome"
+)
+
+func main() {
+	service, err := selenium.NewChromeDriverService("./chromedriver.exe", 4444)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	defer service.Stop()
+
+	caps := selenium.Capabilities{}
+	caps.AddChrome(chrome.Capabilities{Args: []string{"--headless"}})
+
+	driver, err := selenium.NewRemote(caps, "")
+
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	err = driver.Get("https://www.selenium.dev/selenium/web/inputs.html")
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	elements, err := driver.FindElements(selenium.ByName, "checkbox_input")
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	for _, element := range elements {
+		selected, err := element.IsSelected()
+		if err != nil {
+			log.Fatal("Error:", err)
+		}
+
+		if selected {
+			log.Printf("✅ Element is selected")
+		} else {
+			log.Fatalf("❌ Element is not selected")
+		}
+
+		log.Printf("Unselect element")
+		err = element.Click()
+		if err != nil {
+			log.Fatal("Error:", err)
+		}
+
+		selected, err = element.IsSelected()
+		if err != nil {
+			return
+		}
+
+		if !selected {
+			log.Printf("✅ Not selected")
+		} else {
+			log.Fatalf("❌ Element is selected")
+		}
+
+	}
+}
